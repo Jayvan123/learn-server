51568e41f506440671214f9f2e817f5e5a9d131c
Jan Paul <katsuragik919@gmail.com>
2025-05-16 01:29:49 +0800

    test

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:3000", 
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
