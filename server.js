13d6b02f538a7da0efa2b257f3fa57104afc6356
katsura919 <katsuragik919@gmail.com>
2025-05-22 22:54:58 +0800

    update api base url

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000","https://learn-web-aigs.onrender.com", "https://thinkbox.vercel.app/"], 
+    origin: ["http://localhost:3000","https://learn-server-fmbt.onrender.com", "https://learn-web-aigs.onrender.com", "https://thinkbox.vercel.app"], 
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
